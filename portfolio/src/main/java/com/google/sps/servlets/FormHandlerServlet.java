eb4b5ad284d7707b7a0daf7ffa5dd7fc2f788177
Santiago Serrano <santiagoserrano334@gmail.com>
2022-06-10 23:24:49 +0000

    Finish Week 2 tasks. Contact form working.

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -0,0 +1,28 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the value entered in the form.
+    String name = request.getParameter("name");
+    String reason = request.getParameter("reason");
+    String email = request.getParameter("email");
+    String message = request.getParameter("message");
+
+    // Print the value so you can see it in the server logs.
+    System.out.println("SOMEONE CONTACTED! {name: " + name + 
+                       "}, {reason: " + reason + "}, {email: " + email +
+                       "}, {message: " + message + "}");
+
+    response.sendRedirect("index.html");
+  }
+}
