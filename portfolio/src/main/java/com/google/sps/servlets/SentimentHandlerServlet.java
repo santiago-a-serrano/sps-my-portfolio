73d41d51f7dd9e87dbda8fa2e316b92baadf7136
Santiago Serrano <santiagoserrano334@gmail.com>
2022-06-20 15:39:17 +0000

    Now sentiment analysis response has the same theme as all the page

--- portfolio/src/main/java/com/google/sps/servlets/SentimentHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/SentimentHandlerServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
+
 @WebServlet("/sentiment")
 public class SentimentHandlerServlet extends HttpServlet {
     
@@ -23,10 +24,32 @@ public class SentimentHandlerServlet extends HttpServlet {
         String scoreString = Float.toString(score);
         languageService.close();
 
+
+        //TODO: This is probably very inefficient (it's writing every line of the .html file "manually")
         response.setContentType("text/html;");
+
+        //Base HTML:
+        response.getWriter().println("<!DOCTYPE html>");
+        response.getWriter().println("<html>");
+        response.getWriter().println("  <head>");
+        response.getWriter().println("    <meta charset=\"UTF-8\">");
+        response.getWriter().println("    <title>Message received!</title>");
+        response.getWriter().println("    <link id=\"bootstrap-stylesheet\" rel=\"stylesheet\" href=\"style/bootstrap.min-light.css\">");
+        response.getWriter().println("    <link rel=\"stylesheet\" href=\"style/style.css\">");
+        response.getWriter().println("    <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Montserrat\">");
+        response.getWriter().println("    <script src=\"script.js\"></script>");
+        response.getWriter().println("  </head>");
+        response.getWriter().println("  <body>");
+        response.getWriter().println("    <div id=\"content\">");
+
+        //Actual stuff:
         response.getWriter().println("<h2>Your message:</h2>");
         response.getWriter().println("<p>" + message + "</p>");
         response.getWriter().println("<h3>Got the following score:</h3>");
         response.getWriter().println("<p>" + scoreString + "</p>");
+
+        //End HTML:
+        response.getWriter().println("</body>");
+        response.getWriter().println("</html>");
     }
 }
